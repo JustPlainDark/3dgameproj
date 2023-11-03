07afe1785a4981d1a34d1a7be173f896e455add0
justplaindark <dg533@njit.edu>
2023-11-02 21:47:35 -0400

    Pushing up characters and basic changes

--- src/agumon.c
+++ src/agumon.c
@@ -17,9 +17,9 @@ Entity *agumon_new(Vector3D position)
         slog("UGH OHHHH, no agumon for you!");
         return NULL;
     }
-    ent->selectedColor = gfc_color(0.1,1,0.1,1);
+    //ent->selectedColor = gfc_color(0.1,1,0.1,1);
     ent->color = gfc_color(1,1,1,1);
-    ent->model = gf3d_model_load("models/dino.model");
+    ent->model = gf3d_model_load("models/kiryu.model");
     ent->think = agumon_think;
     ent->update = agumon_update;
     vector3d_copy(ent->position,position);
@@ -33,8 +33,8 @@ void agumon_update(Entity *self)
         slog("self pointer not provided");
         return;
     }
-    vector3d_add(self->position,self->position,self->velocity);
-    self->rotation.z += 0.01;
+    //vector3d_add(self->position,self->position,self->velocity);
+    //self->rotation.z += 0.01;
 }
 
 void agumon_think(Entity *self)
